de1d70ffc6491a1256af07e892b8a679db0d7663
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-25 23:18:54 +0530

    html structure is setup

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 // ****** SELECT ITEMS **********
+const input = document.querySelector('input')
+const form = document.querySelector('form')
+const ul = document.querySelector('ul')
 
 // edit option
 
 // ****** EVENT LISTENERS **********
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+
+  if (!input.value.trim()) return
+
+  const listItem = document.createElement('li')
+  listItem.textContent = input.value
+  ul.appendChild(listItem)
+
+  input.value = ''
+})
 
 // ****** FUNCTIONS **********
 
