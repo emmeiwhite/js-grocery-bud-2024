0875ddd7e3e918d2d2b488acbd1addde5c8c7fe3
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-26 11:54:09 +0530

    Trying to work on logic including the editing

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Form DOM Element
 const groceryForm = document.querySelector('.grocery-form')
+const submitBtn = document.querySelector('.submit-btn')
 const alert = document.querySelector('.alert')
 const input = document.querySelector('#grocery')
 
@@ -12,5 +13,24 @@ const clearAllBtn = document.querySelector('.clear-btn')
 
 // Declaring some variables for editing the list
 let editElement
-let editFlag = false
+let editFlag = true
 let editID
+
+/** --- Now Setting up Event Listeners  --- */
+
+function handleSubmit(e) {
+  e.preventDefault()
+
+  const value = input.value
+  const id = new Date().getTime().toString()
+
+  if (value && !editFlag) {
+    console.log('add item to the list')
+  } else if (value && editFlag) {
+    console.log('editing')
+  } else {
+    console.log('empty value')
+  }
+}
+
+groceryForm.addEventListener('submit', handleSubmit)
